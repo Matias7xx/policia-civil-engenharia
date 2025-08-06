b2ac68cc5907dc808fabbfcc65b13b86e0fb607d
Gabriel Matias da Nóbrega <gabriel.nobrega@pc.pb.gov.br>
2025-08-06 11:24:23 -0300

    vite config e env.example

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { globSync } from 'glob';
@@ -6,43 +6,46 @@ import { globSync } from 'glob';
 // Todas as páginas para garantir que estejam no manifesto
 const pageFiles = globSync('resources/js/Pages/**/*.vue');
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/app.js', 
-                'resources/css/app.css',
-                ...pageFiles // FORÇAR inclusão de todas as páginas
-            ],
-            refresh: true,
-            publicDirectory: 'public',
-            buildDirectory: 'build',
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+    export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/js/app.js',
+                    'resources/css/app.css',
+                    ...pageFiles // FORÇAR inclusão de todas as páginas
+                ],
+                refresh: true,
+                publicDirectory: 'public',
+                buildDirectory: 'build',
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': '/resources/js',
+            },
+        },
+        build: {
+            chunkSizeWarningLimit: 2000,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vendor: ['vue', '@inertiajs/vue3', 'axios'],
+                        maps: ['leaflet'],
+                    },
                 },
             },
-        }),
-    ],
-    build: {
-        chunkSizeWarningLimit: 2000, // Aumentar limite para aceitar bundle maior
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    // separar vendor (bibliotecas externas)
-                    vendor: ['vue', '@inertiajs/vue3', 'axios'],
-                    // Leaflet separado (biblioteca grande)
-                    maps: ['leaflet']
-                }
-            }
-        }
-    },
-    resolve: {
-        alias: {
-            '@': '/resources/js',
         },
-    },
+        base: env.VITE_BASE_PATH || '/', //base dinâmica
+    };
 });
