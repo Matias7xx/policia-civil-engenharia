4cdaa38e53b1a0761e3f0cdd47a44d17abaad99c
Gabriel <matiasnobrega7@gmail.com>
2026-03-31 15:32:08 -0300

    correção Mixed Content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (str_starts_with(config('app.url'), 'https')) {
-        URL::forceScheme('https');
+        URL::forceRootUrl(config('app.url'));
         }
     }
 }
